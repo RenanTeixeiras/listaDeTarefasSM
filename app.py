b32c984e075dab953f85d39ec604c3793b21f3e5
Renan_Teixeira <renanteixeiras@hotmail.com>
2023-01-29 14:16:49 -0300

    Feito o projeto. Com front-end básico. Estilização virá em breve.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import date
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
+app.config['SECRET_KEY'] = 'aleatoryOne'
 db= SQLAlchemy(app)
 app.app_context().push()
 login_manager = LoginManager(app)
@@ -18,12 +19,12 @@ def current_user(user_id):
 class User(db.Model, UserMixin):
     __tablename__ = "usuarios"
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(84), nullable=False)
-    email = db.Column(db.String(84), nullable=False, unique=True, index=True)
+    nome = db.Column(db.String(84), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(84), nullable=False, unique=True)
     senha = db.Column(db.String(255), nullable=False)
 
     def __str__(self):
-        return self.name
+        return self.nome
 
 class Task(db.Model):
     __tablename__ = "Tarefas"
@@ -44,9 +45,9 @@ def login():
         senha = request.form['senha']
 
         user = User.query.filter_by(nome=nome).first()
-
+        print(user.nome, user.senha)
         if not user:
-            flash('Credenciais Incorretas')
+            flash('Credenciais Incorretas aqui')
             return redirect(url_for('login'))
 
         if not check_password_hash(user.senha, senha):
@@ -54,7 +55,7 @@ def login():
             return redirect(url_for('login'))
 
         login_user(user)
-        return redirect(url_for('users'))
+        return redirect(url_for('tarefas'))
 
 
     return render_template('login.html')
@@ -65,15 +66,14 @@ def cadastro():
         user = User()
         user.email = request.form['email']
         user.nome = request.form['usuario']
-        user.senha = request.form['senha']
+        user.senha = generate_password_hash(request.form['senha'])
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
     return render_template('cadastro.html')
 
-
 @app.route("/tarefas", methods=['GET', 'POST'])
-# @login_required
+@login_required
 def tarefas():
     if request.method == "POST":
         tarefa = Task()
@@ -88,14 +88,33 @@ def tarefas():
     return render_template("lista.html", tasks=tarefas) 
 
 @app.route('/deletar/<int:id>')
+@login_required
 def deletar_tarefa(id):
     tarefa = Task.query.filter_by(id_tarefa=id).first()
     db.session.delete(tarefa)
     db.session.commit()
     return redirect(url_for('tarefas'))
 
+@app.route('/deletarUsr/<int:id>')
+@login_required
+def deletar_usuario(id):
+    usuario = User.query.filter_by(id=id).first()
+    db.session.delete(usuario)
+    db.session.commit()
+    return redirect(url_for('usuarios'))
+
+@app.route("/usuarios")
+@login_required
+def usuarios():
+    users = User.query.all() #SELECT * FROM USERS
+    return render_template("users.html", users=users)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
-    db.create_all()
     app.run(debug=True)
